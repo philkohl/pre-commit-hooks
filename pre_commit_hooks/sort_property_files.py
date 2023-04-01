00d067559474ad5d42c0f3dff17a1d63ceaa793e
philkohl <daniel.philipp.kohl@gmail.com>
2023-04-01 11:31:18 +0200

    fix

--- pre_commit_hooks/sort_property_files.py
+++ pre_commit_hooks/sort_property_files.py
@@ -7,7 +7,13 @@ from pre_commit_hooks.util import added_files
 
 class PropertyEntry:
     def __init__(self, input: str):
-        self.key, self.value = input.split("=")
+        splitted = input.split("=")
+
+        if len(splitted) < 2:
+            raise Exception(f"'{input}' not well formatted!")
+
+        self.key = splitted[0]
+        self.value = "=".join(splitted[1:])
 
     def __lt__(self, other):
         return self.key < other.key
@@ -15,7 +21,7 @@ class PropertyEntry:
 
 def sort_file(filename):
     entries = []
-    with open(filename) as f:
+    with open(filename, encoding="UTF-8") as f:
         lines = [line for line in f]
 
     for l in lines:
@@ -23,7 +29,7 @@ def sort_file(filename):
 
     sorted_entries = sorted(entries)
 
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="UTF-8") as f:
         f.writelines([f"{e.key}={e.value}" for e in sorted_entries])
 
 
